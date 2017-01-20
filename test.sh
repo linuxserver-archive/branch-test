f333599991eeaf521373c5188d576fdaa62702c7
sparklyballs <sparklyballs@gmail.com>
2017-01-20 11:06:42 +0000

    blah

--- test.sh
+++ test.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-unset TAG PAYLOAD
+unset HUB_REPO HUB_TOKEN HUB_URL PAYLOAD TAG
 
-TAG='1.1'
+TAG='1.0'
 PAYLOAD='{"source_type": "Tag", "source_name": "'"$TAG"'"}'
 
-curl -H "Content-Type: application/json" --data "${PAYLOAD}" -X POST https://registry.hub.docker.com/u/linuxserver/branch-test/trigger/bbee250e-0b92-40d7-a339-8180c27d2d1e/
+HUB_REPO="linuxserver/branch-test"
+HUB_TOKEN="bbee250e-0b92-40d7-a339-8180c27d2d1e"
+HUB_URL="https://registry.hub.docker.com/u/${HUB_REPO}/trigger/${HUB_TOKEN}/"
+
+
+curl -H "Content-Type: application/json" --data "${PAYLOAD}" -X POST "${HUB_URL}"
 
